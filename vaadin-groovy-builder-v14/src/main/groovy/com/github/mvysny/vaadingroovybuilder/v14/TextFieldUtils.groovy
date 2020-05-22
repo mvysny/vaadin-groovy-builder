066606974769f815345aa9578eef10654b24eb56
Martin Vysny <mavi@vaadin.com>
2020-05-22 15:37:06 +0300

    add text selection utility methods to TextField/TextArea

--- vaadin-groovy-builder-v14/src/main/groovy/com/github/mvysny/vaadingroovybuilder/v14/TextFieldUtils.groovy
+++ vaadin-groovy-builder-v14/src/main/groovy/com/github/mvysny/vaadingroovybuilder/v14/TextFieldUtils.groovy
@@ -3,6 +3,8 @@ package com.github.mvysny.vaadingroovybuilder.v14
 import com.vaadin.flow.component.HasComponents
 import com.vaadin.flow.component.textfield.BigDecimalField
 import com.vaadin.flow.component.textfield.EmailField
+import com.vaadin.flow.component.textfield.GeneratedVaadinTextArea
+import com.vaadin.flow.component.textfield.GeneratedVaadinTextField
 import com.vaadin.flow.component.textfield.IntegerField
 import com.vaadin.flow.component.textfield.NumberField
 import com.vaadin.flow.component.textfield.PasswordField
@@ -12,6 +14,7 @@ import groovy.transform.CompileStatic
 import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.Nullable
 
+import static com.github.mvysny.vaadingroovybuilder.v14.Utils.require
 import static com.github.mvysny.vaadingroovybuilder.v14.VaadinDsl.init
 
 /**
@@ -92,5 +95,85 @@ class TextFieldUtils {
     static BigDecimalField bigDecimalField(@NotNull HasComponents self, @Nullable String label = null,
                                            @DelegatesTo(value = BigDecimalField, strategy = Closure.DELEGATE_FIRST) @NotNull Closure block) {
         init(self, new BigDecimalField(label), block)
+    }
+
+    /**
+     * Selects all text in this text field. The selection is not really visible in
+     * the browser unless the field is focused.
+     */
+    static void selectAll(@NotNull GeneratedVaadinTextField<?, String> self) {
+        self.getElement().executeJs("this.inputElement.select()");
+    }
+
+    /**
+     * Clears the selection in the text field and moves the cursor to the end of
+     * the text. There may be no effect if the field is unfocused - the browser
+     * generally selects all when the field gains focus.
+     */
+    static void selectNone(@NotNull GeneratedVaadinTextField<?, String> self) {
+        setCursorLocation(self, self.getValue()?.length() ?: 0)
+    }
+
+    /**
+     * Moves the cursor within the text field. Has the side-effect of clearing the selection.
+     * <p></p>
+     * There may be no effect if the field is unfocused - the browser
+     * generally selects all when the field gains focus.
+     */
+    static void setCursorLocation(@NotNull GeneratedVaadinTextField<?, String> self,
+                                  int cursorLocation) {
+        select(self, cursorLocation, cursorLocation)
+    }
+
+    /**
+     * Selects given characters in this text field. The selection is not really visible in
+     * the browser unless the field is focused.
+     */
+    static void select(@NotNull GeneratedVaadinTextField<?, String> self,
+            int selectionStart,
+            int selectionEnd) {
+        require(selectionStart >= 0) { "selectionStart: must be 0 or greater but was $selectionStart" }
+        require(selectionEnd >= 0) { "selectionEnd: must be 0 or greater but was $selectionEnd" }
+        self.getElement().executeJs("this.inputElement.setSelectionRange($selectionStart, $selectionEnd)")
+    }
+
+    /**
+     * Selects all text in this text field. The selection is not really visible in
+     * the browser unless the field is focused.
+     */
+    static void selectAll(@NotNull GeneratedVaadinTextArea<?, String> self) {
+        self.getElement().executeJs("this.inputElement.select()");
+    }
+
+    /**
+     * Clears the selection in the text field and moves the cursor to the end of
+     * the text. There may be no effect if the field is unfocused - the browser
+     * generally selects all when the field gains focus.
+     */
+    static void selectNone(@NotNull GeneratedVaadinTextArea<?, String> self) {
+        setCursorLocation(self, self.getValue()?.length() ?: 0)
+    }
+
+    /**
+     * Moves the cursor within the text field. Has the side-effect of clearing the selection.
+     * <p></p>
+     * There may be no effect if the field is unfocused - the browser
+     * generally selects all when the field gains focus.
+     */
+    static void setCursorLocation(@NotNull GeneratedVaadinTextArea<?, String> self,
+                                  int cursorLocation) {
+        select(self, cursorLocation, cursorLocation)
+    }
+
+    /**
+     * Selects given characters in this text field. The selection is not really visible in
+     * the browser unless the field is focused.
+     */
+    static void select(@NotNull GeneratedVaadinTextArea<?, String> self,
+                       int selectionStart,
+                       int selectionEnd) {
+        require(selectionStart >= 0) { "selectionStart: must be 0 or greater but was $selectionStart" }
+        require(selectionEnd >= 0) { "selectionEnd: must be 0 or greater but was $selectionEnd" }
+        self.getElement().executeJs("this.inputElement.setSelectionRange($selectionStart, $selectionEnd)")
     }
 }
