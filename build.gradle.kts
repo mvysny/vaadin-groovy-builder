29a595965d18ef816076f1e70aef0057f2df7789
Martin Vysny <mavi@vaadin.com>
2021-06-18 09:02:09 +0200

    0.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.vaadin-groovy-builder"
-    version = "0.0.9-SNAPSHOT"
+    version = "0.0.9"
 
     repositories {
         mavenCentral()
