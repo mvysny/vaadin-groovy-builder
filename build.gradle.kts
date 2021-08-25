b6e1343945a2f1396cafb5e7108b6b47220b1721
Martin Vysny <mavi@vaadin.com>
2021-08-25 09:53:06 +0300

    dependency v-bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.*
 plugins {
     id("java-library")
     groovy
-    id("org.gretty") version "3.0.4"
+    id("org.gretty") version "3.0.6"
     `maven-publish`
     id("com.vaadin") version "0.14.6.0" apply(false)
     signing
@@ -18,7 +18,6 @@ allprojects {
 
     repositories {
         mavenCentral()
-        jcenter()  // because of gretty-runner-jetty94
     }
 
     tasks {
