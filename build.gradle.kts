9654b173353b0d3077bb8a28e7905836fe61c734
Martin Vysny <mavi@vaadin.com>
2023-11-20 09:03:34 +0200

    build.gradle: minor refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,25 +67,25 @@ subprojects {
                     this.artifactId = artifactId
                     version = project.version.toString()
                     pom {
-                        description.set("Vaadin Groovy Builder, Groovy extensions/DSL for Vaadin")
-                        name.set(artifactId)
-                        url.set("https://github.com/mvysny/vaadin-groovy-builder")
+                        description = "Vaadin Groovy Builder, Groovy extensions/DSL for Vaadin"
+                        name = artifactId
+                        url = "https://github.com/mvysny/vaadin-groovy-builder"
                         licenses {
                             license {
-                                name.set("The MIT License (MIT)")
-                                url.set("https://opensource.org/licenses/MIT")
-                                distribution.set("repo")
+                                name = "The MIT License (MIT)"
+                                url = "https://opensource.org/licenses/MIT"
+                                distribution = "repo"
                             }
                         }
                         developers {
                             developer {
-                                id.set("mavi")
-                                name.set("Martin Vysny")
-                                email.set("martin@vysny.me")
+                                id = "mavi"
+                                name = "Martin Vysny"
+                                email = "martin@vysny.me"
                             }
                         }
                         scm {
-                            url.set("https://github.com/mvysny/vaadin-groovy-builder")
+                            url = "https://github.com/mvysny/vaadin-groovy-builder"
                         }
                     }
 
